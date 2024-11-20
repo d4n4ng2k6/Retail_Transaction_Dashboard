e94b3bb506ff4bc1a7ac8b13af3244519719c321
Danang Aditya Nugraha <38489648+d4n4ng2k6@users.noreply.github.com>
2024-11-20 04:01:02 +0000

    Load dataset and view basic info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.title("Retail Transaction Data Dashboard")
 st.write(
     "Here is Data Analysis Dashboard from Retail-Transaction dataset. This dataset consist of 30000 entries with 13 column"
 )
+df = pd.read_csv("Retail_Transactions_Dataset.csv")
+st.write("Basic ")
+st.write(df)
